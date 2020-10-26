0d97d6ec4f090bf48bf5136e2a6cdb0c0476f72b
hoonv <chaeefe@gmail.com>
2020-10-26 18:09:14 +0900

    feat: iOS project init
    - iOS 프로젝트 생성

---
+++ iOS/IssueTracker/IssueTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IssueTracker
+//
+//  Created by 채훈기 on 2020/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
