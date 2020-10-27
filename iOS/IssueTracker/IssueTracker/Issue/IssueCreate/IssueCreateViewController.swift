d59c7015103ce18e7dc85bcd67c4007a30ebead0
hoonv <chaeefe@gmail.com>
2020-10-27 15:59:54 +0900

    feat: create issue controllers & layout (#6)
    - 이슈와 관련된 뷰 컨트롤러 생성
    - 메인 이슈 뷰의 레이아웃 적용
    - 페어프로그래밍

---
+++ iOS/IssueTracker/IssueTracker/Issue/IssueCreate/IssueCreateViewController.swift
@@ -0,0 +1,15 @@
+//
+//  IssueCreateViewController.swift
+//  IssueTracker
+//
+//  Created by 채훈기 on 2020/10/27.
+//
+
+import UIKit
+
+class IssueCreateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ iOS/IssueTracker/IssueTracker/Issue/IssueViewController.swift
@@ -0,0 +1,16 @@
+//
+//  IssueViewController.swift
+//  IssueTracker
+//
+//  Created by 채훈기 on 2020/10/27.
+//
+
+import UIKit
+
+class IssueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
