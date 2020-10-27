6ebd3379db383bdf413217adb20745d149e2a9ab
sbyeol3 <ahnsbyeol3@gmail.com>
2020-10-27 14:25:48 +0900

    feat : update eslint configuration
    - prettier 설정 다시 함-> 잘 되는데 eslint가 이상함

--- server/app.js
+++ server/app.js
@@ -12,11 +12,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
