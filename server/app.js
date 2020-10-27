f8075e7687f21431fb43fb8c4e6095ae55960160
sbyeol3 <ahnsbyeol3@gmail.com>
2020-10-27 15:04:22 +0900

    fix : fix eslint error
    - eslint 버전 문제..
    - id-denylist 모듈을 찾을 수 없다고 로그에 떠서 업그레이드

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sequelize = require('sequelize');
+
 const app = express();
 
 app.use(logger('dev'));
