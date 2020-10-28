a5cd26fd2e997e0b2d8a38bbbe647e4f68c888ab
hoonv <chaeefe@gmail.com>
2020-10-28 23:12:01 +0900

    feat: create criteria for filter
    - 필터를 위한 조건 생성

---
+++ iOS/IssueTracker/IssueTracker/Issue/IssueList/Model/Criteria.swift
@@ -0,0 +1,74 @@
+//
+//  Criteria.swift
+//  IssueTracker
+//
+//  Created by 채훈기 on 2020/10/28.
+//
+
+import Foundation
+
+protocol IssueCriteria {
+    
+    func apply(issues: [Issue]) -> [Issue]
+}
+
+class CloseCriteria: IssueCriteria {
+    
+    func apply(issues: [Issue]) -> [Issue] {
+        return issues.filter { $0.state == 0 }
+    }
+}
+
+class OpenCriteria: IssueCriteria {
+    
+    func apply(issues: [Issue]) -> [Issue] {
+        return issues.filter { $0.state == 1 }
+    }
+}
+
+class WriterCriteria: IssueCriteria {
+    
+    let inputText: String
+    
+    init(input: String) {
+        inputText = input
+    }
+    
+    func apply(issues: [Issue]) -> [Issue] {
+        return issues.filter { $0.title.contains(inputText)}
+    }
+}
+
+class AndCriteria: IssueCriteria {
+    
+    let left: IssueCriteria
+    let right: IssueCriteria
+    
+    init(left: IssueCriteria, right: IssueCriteria) {
+        self.left = left
+        self.right = right
+    }
+    
+    func apply(issues: [Issue]) -> [Issue] {
+        let leftResult = left.apply(issues: issues)
+        return right.apply(issues: leftResult)
+    }
+}
+
+class OrCriteria: IssueCriteria {
+    
+    let left: IssueCriteria
+    let right: IssueCriteria
+    
+    init(left: IssueCriteria, right: IssueCriteria) {
+        self.left = left
+        self.right = right
+    }
+    
+    func apply(issues: [Issue]) -> [Issue] {
+        let leftResult = left.apply(issues: issues)
+        let rightResult = right.apply(issues: issues)
+ 
+        return Array(Set(leftResult + rightResult))
+    }
+}
