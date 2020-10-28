e53ae6f571f090b05a34472cad91433658325b99
Taegyung Gong <gtg92t@gmail.com>
2020-10-28 21:27:45 +0900

    Feat: Implement jwt middleware(#21) - jsonwebtoken & passport-jwt module 설치 - generateToken util 함수 생성 - 사용자가 누구인지 token을 통해 확인하는 middleware 구현 - id로 user를 찾는 model 함수 구현

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const passport = require('passport');
+const { generateToken } = require('../utils/auth');
 
 const router = express.Router();
 
 router.get('/github/login', passport.authenticate('github'));
 
 router.get('/github', passport.authenticate('github', { session: false }), (req, res) => {
-  // TODO: jwt 만들어주기
-  res.json({ message: 'hihi' });
+  const token = generateToken(req.id, req.username);
+  res.status(200).json({ message: 'success', token });
 });
 
 module.exports = router;
