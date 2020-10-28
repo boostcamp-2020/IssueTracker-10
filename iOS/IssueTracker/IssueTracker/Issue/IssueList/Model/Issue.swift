914df2b5bf954f1c14cf59ab462f351f399351d1
강민석 <kms@kati-2.local>
2020-10-28 22:53:54 +0900

    feat: Issue객체를 struct에서 class로 변경
    state를 변경할 때 class로 참조하는게 낫다

--- iOS/IssueTracker/IssueTracker/Issue/IssueList/Model/Issue.swift
+++ iOS/IssueTracker/IssueTracker/Issue/IssueList/Model/Issue.swift
@@ -7,15 +7,35 @@
 
 import Foundation
 
-struct Issue: Hashable {
+class Issue: Hashable {
+	static func == (lhs: Issue, rhs: Issue) -> Bool {
+		return lhs.id == rhs.id
+	}
+	
+	func hash(into hasher: inout Hasher) {
+		hasher.combine(id)
+	}
+	
 	let id: Int
 	let title: String
 	let author: Int
-	let state: Int
+	var state: Int
 	let milestoneId: Int
 	let createdAt: Date
 	let updatedAt: Date
 	
 	let description: String // firstComment
 	let labels: [String]
+	
+	init(id: Int, title: String, author: Int, state: Int, milestoneId: Int, createdAt: Date, updatedAt: Date, description: String, labels: [String]) {
+		self.id = id
+		self.title = title
+		self.author = author
+		self.state = state
+		self.milestoneId = milestoneId
+		self.createdAt = createdAt
+		self.updatedAt = updatedAt
+		self.description = description
+		self.labels = labels
+	}
 }
