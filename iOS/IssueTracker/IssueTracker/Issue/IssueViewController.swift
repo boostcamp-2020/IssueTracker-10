df435e78b7ba14923b204deae5e04da6b2151ee8
hoonv <chaeefe@gmail.com>
2020-10-28 14:14:18 +0900

    feat: search bar delegate
    - 서치바의 델리게이트 구현

--- iOS/IssueTracker/IssueTracker/Issue/IssueViewController.swift
+++ iOS/IssueTracker/IssueTracker/Issue/IssueViewController.swift
@@ -9,8 +9,25 @@ import UIKit
 
 class IssueViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
 
+extension IssueViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // inputText
+    }
+
+    func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
+        navigationController?.setNavigationBarHidden(false, animated: false)
+        return true
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+}
