e5dd8bb26d7395377d6c8e4b7d0e68c847c9509a
hoonv <chaeefe@gmail.com>
2020-10-29 15:46:58 +0900

    feat: divide datasource from VC
    - 뷰컨으로 부터 데이터소스를 분리

---
+++ iOS/IssueTracker/IssueTracker/Issue/IssueList/IssueDataSource.swift
@@ -0,0 +1,60 @@
+//
+//  IssueDataSource.swift
+//  IssueTracker
+//
+//  Created by 채훈기 on 2020/10/29.
+//
+
+import UIKit
+
+class IssueDiffableDataSource {
+
+    var dataSource: UICollectionViewDiffableDataSource<Section, Issue>!
+    let collectionView: UICollectionView
+    
+    init(collectionView: UICollectionView) {
+        self.collectionView = collectionView
+        configureDataSource()
+    }
+    
+    enum Section: CaseIterable {
+        case main
+    }
+    
+    struct Identifier {
+        static let issueCell = "issueCell"
+    }
+    
+    func configureDataSource() {
+        let cellProvider = { (collectionView: UICollectionView, indexPath: IndexPath, issue: Issue) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Identifier.issueCell, for: indexPath)
+            if let issueCell = cell as? IssueCollectionViewCell {
+                issueCell.configure(issue: issue)
+                return cell
+            }
+            return nil
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, Issue>(collectionView: collectionView, cellProvider: cellProvider)
+    }
+    
+    func performQuery(issues: [Issue], filter: IssueCriteria?) {
+        
+        var data = issues
+        if let filter = filter {
+            data = filter.apply(issues: issues)
+        }
+        updateDataSource(issues: data)
+    }
+    
+    func updateDataSource(issues: [Issue]) {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Issue>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(issues)
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
+    
+    func itemIdentifier(for indexPath: IndexPath) -> Issue? {
+        return dataSource.itemIdentifier(for: indexPath)
+    }
+}
