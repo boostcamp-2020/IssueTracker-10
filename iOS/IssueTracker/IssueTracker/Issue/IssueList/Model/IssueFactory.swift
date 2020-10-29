c93780d3c92d8b0ea5021d54bbb876808fe37f10
hoonv <chaeefe@gmail.com>
2020-10-29 18:44:28 +0900

    chore: apply indentation
    - 띄어쓰기

--- iOS/IssueTracker/IssueTracker/Issue/IssueList/Model/IssueFactory.swift
+++ iOS/IssueTracker/IssueTracker/Issue/IssueList/Model/IssueFactory.swift
@@ -15,23 +15,23 @@ class IssueFactory {
     }
     
     private static var titles = ["first제목이 미친듯이 길면 안되나보네 그러면 이거는",
-                   "seconde Title",
-                   "레이블 목록 보기 구현",
-                   "iOS는 뭘 할까요",
-                   "Web은 뭘 할까요",
-                   "스크롤을위해1",
-                   "스크롤을위해2",
-                   "스크롤을위해3",
-                   ]
+                                 "seconde Title",
+                                 "레이블 목록 보기 구현",
+                                 "iOS는 뭘 할까요",
+                                 "Web은 뭘 할까요",
+                                 "스크롤을위해1",
+                                 "스크롤을위해2",
+                                 "스크롤을위해3",
+    ]
     
     private static var desc = ["firstDescription",
-                "secondDescription\n이건좀 길지 않나요 점점점으로 표시가 될까요 알아맞춰보세요 딩동댕동",
-                "레이블 전체목록을 볼 수 있어야 한다.\n2줄까지 보입니다.\n이건 안보여",
-                "DiffableDataSource\n너무 어려워요",
-                "No description\nNo descriptoin",
-                "hello world",
-                "import foundaion",
-                "DiffableDataSource\n너무 어려워요",
+                               "secondDescription\n이건좀 길지 않나요 점점점으로 표시가 될까요 알아맞춰보세요 딩동댕동",
+                               "레이블 전체목록을 볼 수 있어야 한다.\n2줄까지 보입니다.\n이건 안보여",
+                               "DiffableDataSource\n너무 어려워요",
+                               "No description\nNo descriptoin",
+                               "hello world",
+                               "import foundaion",
+                               "DiffableDataSource\n너무 어려워요",
     ]
     
     private static var labels = [["ios"],["web"],["web"],["web"],["web"],["web"],["web"],["web"]]
