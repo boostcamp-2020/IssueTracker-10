e9513afc8ff7ce32a59e48e678fd4dbe8731aab6
sbyeol3 <ahnsbyeol3@gmail.com>
2020-10-29 11:52:46 +0900

    Test : add test code and add Script for testing (#2)
    - create issue 테스트 코드 추가
    - 테스트 실행을 위한 스크립트 추가 (npm run test)

--- server/test/issue.spec.js
+++ server/test/issue.spec.js
@@ -25,6 +25,27 @@ describe('POST /api/issue API는', () => {
         }
       });
   });
+  it('타이틀이 존재하지 않으면 400을 리턴한다.', (done) => {
+    request(app)
+      .post('/api/issue')
+      .set({
+        Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+        'Content-Type': 'application/json',
+      })
+      .send({
+        content: 'null',
+      })
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .end((err, result) => {
+        if (err) {
+          done(err);
+        } else {
+          done();
+          console.log(result.body);
+        }
+      });
+  });
   it('정상적일 경우에는 200 코드와 json을 리턴한다.', (done) => {
     request(app)
       .post('/api/issue')
@@ -35,9 +56,6 @@ describe('POST /api/issue API는', () => {
       .send({
         title: 'issue example 1',
         content: null,
-        label: [],
-        assignees: [],
-        milestone: [],
       })
       .expect('Content-Type', /json/)
       .expect(200)
