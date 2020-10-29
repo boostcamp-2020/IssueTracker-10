8f72800438b8740fb5cdc57fcf4b7aad4c6ae3fa
sbyeol3 <ahnsbyeol3@gmail.com>
2020-10-29 11:21:35 +0900

    Feat : add API router (#2)
    - /api route로 오는 요청에 대해 라우팅 처리
    - index에서 /api에 대한 모든 리퀘스트는 jwt 미들웨어를 통과하게끔 코드 추가
    - issue 라우터 파일 추가

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 const authRouter = require('./auth');
+const apiRouter = require('./api');
+const authenticateJwt = require('../middlewares/authenticateJwt');
 
 router.use('/auth', authRouter);
+router.use('/api', authenticateJwt, apiRouter);
 
 module.exports = router;
