b7dc60575a4cb2e01f451e54a8e363d341b3e370
kati-kms <qkrguswndk@naver.com>
2020-11-02 16:18:17 +0900

    feat: Profile View 추가
    로그인 완료된 경우 Profile View에서 프로필 정보를  보여준다

---
+++ iOS/IssueTracker/IssueTracker/Profile/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  IssueTracker
+//
+//  Created by 강민석 on 2020/11/02.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+	
+	@IBOutlet weak var name: UILabel!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
