d6b06b2ec2fa5973da0d4a71ee35a1966f173cb4
Taegyung Gong <gtg92t@gmail.com>
2020-11-02 12:38:32 +0900

    Feat: Add toggle milestone state router - milestone state toggle api 추가

--- server/controllers/api/milestone.js
+++ server/controllers/api/milestone.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const milestoneService = require('../../services/milestone');
 
 router.delete('/:milestoneId', milestoneService.deleteMilestone);
+router.put('/:milestoneId/state', milestoneService.toggleState);
 
 module.exports = router;
