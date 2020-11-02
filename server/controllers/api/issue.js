5bd1004046d76eab5ab383040d5c05878f2f31c0
Taegyung Gong <gtg92t@gmail.com>
2020-11-02 18:15:06 +0900

    Feat: Add create comment controller - /api/issue/:issueId/comment로 comment create api 추가

--- server/controllers/api/issue.js
+++ server/controllers/api/issue.js
@@ -2,11 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 const issueService = require('../../services/issue');
+const commentService = require('../../services/comment');
 
 router.post('/', issueService.createIssue);
 router.get('/:issueId', issueService.selectIssueById);
 router.put('/state', issueService.toggleState);
 router.put('/:issueId', issueService.updateIssueTitle);
 router.delete('/:issueId', issueService.deleteIssue);
+
+router.post('/:issueId/:comment', commentService.createComment);
 
 module.exports = router;
