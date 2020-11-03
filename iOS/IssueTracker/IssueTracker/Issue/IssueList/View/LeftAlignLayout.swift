70f23e2e932c7a4c5d9a9f508ac376880e485144
hoonv <chaeefe@gmail.com>
2020-11-03 22:52:08 +0900

    chore: change class name to LeftAlign
    - 좀더 구체적인 클래스 이름으로 변경

--- iOS/IssueTracker/IssueTracker/Issue/IssueList/View/LeftAlignLayout.swift
+++ iOS/IssueTracker/IssueTracker/Issue/IssueList/View/LeftAlignLayout.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class FlowLayout: UICollectionViewFlowLayout {
+class LeftAlignFlowLayout: UICollectionViewFlowLayout {
 
     required init(minimumInteritemSpacing: CGFloat = 0, minimumLineSpacing: CGFloat = 0, sectionInset: UIEdgeInsets = .zero) {
         super.init()
@@ -43,31 +43,6 @@ class FlowLayout: UICollectionViewFlowLayout {
         }
 
         return layoutAttributes
-    }
-
-}
-
-class LabelCollectionViewCell: UICollectionViewCell {
-
-    let label = UILabel()
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-
-        contentView.backgroundColor = .orange
-        label.preferredMaxLayoutWidth = 120
-        label.numberOfLines = 1
-
-        contentView.addSubview(label)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        contentView.layoutMarginsGuide.topAnchor.constraint(equalTo: label.topAnchor).isActive = true
-        contentView.layoutMarginsGuide.leadingAnchor.constraint(equalTo: label.leadingAnchor).isActive = true
-        contentView.layoutMarginsGuide.trailingAnchor.constraint(equalTo: label.trailingAnchor).isActive = true
-        contentView.layoutMarginsGuide.bottomAnchor.constraint(equalTo: label.bottomAnchor).isActive = true
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 
 }
