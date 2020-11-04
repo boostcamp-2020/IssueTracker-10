54ae66c2103128b7117a2e868c2fbc8cc3df0430
hoonv <chaeefe@gmail.com>
2020-11-04 17:43:35 +0900

    feat: create extension group & files
    - 익스텐션 그룹을 생성후 파일 옮김

---
+++ iOS/IssueTracker/IssueTracker/Extensions/NotificationName.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationName.swift
+//  IssueTracker
+//
+//  Created by 채훈기 on 2020/11/04.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let issueDidChanged = Notification.Name.init("issueDidChanged")
+}
