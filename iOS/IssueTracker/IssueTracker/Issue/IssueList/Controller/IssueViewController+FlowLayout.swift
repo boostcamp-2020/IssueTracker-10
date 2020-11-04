e3b2c600a1cc63932dc0da4e9b1b25273d000c8f
hoonv <chaeefe@gmail.com>
2020-11-04 17:54:43 +0900

    feat: estimate cell size
    - 라벨 길이에 따른 셀 사이즈 측정

--- iOS/IssueTracker/IssueTracker/Issue/IssueList/Controller/IssueViewController+FlowLayout.swift
+++ iOS/IssueTracker/IssueTracker/Issue/IssueList/Controller/IssueViewController+FlowLayout.swift
@@ -10,8 +10,15 @@ import UIKit
 extension IssueViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 170)
+        let maxLine: CGFloat = 2
+        let totalWidth = viewModel.issues[indexPath.row].labels.map {
+            $0.title.widthOfString(usingFont: UIFont.systemFont(ofSize: 13, weight: .semibold))
+            }
+            .map { $0 + CGFloat(40) }
+            .reduce(0) { $0 + $1 }
+        
+        let lines = min(maxLine, ceil(totalWidth / view.frame.width))
+        
+        return CGSize(width: view.frame.width * 0.9, height: 100 + 36 * lines)
     }
-    
-    
 }
