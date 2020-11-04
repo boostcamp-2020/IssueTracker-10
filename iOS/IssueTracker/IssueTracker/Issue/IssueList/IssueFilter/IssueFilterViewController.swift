41892fac211710c63da2d99b3326de5a7a32f6d2
kati-kms <qkrguswndk@naver.com>
2020-11-04 19:52:55 +0900

    feat: configure FilterViewController & cell
    filterViewController Delegate와 DataSource 연결
    FilterViewCell도 연결해서, cell 선택시 check버튼으로 선택중임을
    보여준다.

--- iOS/IssueTracker/IssueTracker/Issue/IssueList/IssueFilter/IssueFilterViewController.swift
+++ iOS/IssueTracker/IssueTracker/Issue/IssueList/IssueFilter/IssueFilterViewController.swift
@@ -8,9 +8,51 @@
 import UIKit
 
 class IssueFilterViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	
+	@IBAction func doneButtonTouched(_ sender: Any) {
+		self.dismiss(animated: true, completion: nil)
+	}
+	
+	@IBAction func cancelButtonTouched(_ sender: Any) {
+		self.dismiss(animated: true, completion: nil)
+	}
+	
+	@IBOutlet weak var filterTableView: UITableView!
+	
+	var dataSource: FilterDiffableDataSource!
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
 		navigationController?.navigationBar.prefersLargeTitles = true
+		filterTableView.delegate = self
+		dataSource = FilterDiffableDataSource(with: filterTableView)
+		filterTableView?.allowsMultipleSelection = true
+	}
+}
+
+extension IssueFilterViewController: UITableViewDelegate {
+	func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+		let sectionKind = FilterDiffableDataSource.Section(rawValue: section)
+		let label = UILabel()
+		label.textColor = .systemGray
+		label.backgroundColor = .systemGray6
+		label.textAlignment = .center
+		label.baselineAdjustment = .alignBaselines
+		label.text = sectionKind?.header
+		return label
+	}
+
+	func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+		return 50
+	}
+	
+	func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+		guard let cell = tableView.cellForRow(at: indexPath) else { return }
+		cell.isSelected = false
+	}
+
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		guard let cell = tableView.cellForRow(at: indexPath) else { return }
+		cell.isSelected = true
 	}
 }
