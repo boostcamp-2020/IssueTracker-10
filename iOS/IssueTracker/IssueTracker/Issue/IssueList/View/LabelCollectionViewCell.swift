f4a6e4f2fdc9cc26b3efa542096c5a8d0a2706c4
hoonv <chaeefe@gmail.com>
2020-11-05 11:22:26 +0900

    feat: update labelCollectionViewCell
    - 레이블 셀 업데이트 
    - 노티피케이션센터 등록

--- iOS/IssueTracker/IssueTracker/Issue/IssueList/View/LabelCollectionViewCell.swift
+++ iOS/IssueTracker/IssueTracker/Issue/IssueList/View/LabelCollectionViewCell.swift
@@ -10,7 +10,13 @@ import UIKit
 class LabelCollectionViewCell: UICollectionViewCell {
 
     let label = UILabel()
-
+    var color: UIColor!
+    var isclicked = false
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
 
@@ -28,19 +34,48 @@ class LabelCollectionViewCell: UICollectionViewCell {
             contentView.layoutMarginsGuide.bottomAnchor.constraint(equalTo: label.bottomAnchor),
             label.widthAnchor.constraint(lessThanOrEqualToConstant: 190)
         ])
-        
+        NotificationCenter.default.addObserver(self, selector: #selector(labelToggled), name: .labelDidToggled, object: nil)
     }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    
+    @objc private func labelToggled(_ notification: Notification) {
+        if let object = notification.object as? [String: Any] {
+            guard let name = object["title"] as? String,
+                  let clicked = object["clicked"] as? Bool,
+                  name == label.text else { return }
+            updateColor(flag: clicked)
+        }
     }
     
     func configure(item: Label) {
-        let color = item.color.hexStringToUIColor()
+        color = item.color.hexStringToUIColor()
         label.text = item.title
-        backgroundColor = color
+        self.layer.borderWidth = 1
+        self.layer.borderColor = color.cgColor
+        label.textColor = color
+        backgroundColor = .white
         layer.cornerRadius = 12
-        label.textColor = color.isDark ? .white : .black
         clipsToBounds = true
+        isclicked = SelectedLabels.shared.contains(title: item.title)
+        updateColor(flag: isclicked)
+    }
+    
+    func toggle() {
+        isclicked = !isclicked
+        updateColor(flag: isclicked)
+    }
+    
+    func updateColor(flag: Bool) {
+        if flag {
+            self.backgroundColor = color
+            self.label.textColor = .white
+            
+        } else {
+            self.backgroundColor = .white
+            self.label.textColor = color
+        }
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
     }
 }
