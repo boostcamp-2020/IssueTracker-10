b280a61d37a652e87156c475ba04f3fa1cbe8e5f
hoonv <chaeefe@gmail.com>
2020-11-05 19:01:34 +0900

    feat: update homeView
    - issue 추가

--- iOS/IssueTracker/IssueTracker/Home/HomeViewController.swift
+++ iOS/IssueTracker/IssueTracker/Home/HomeViewController.swift
@@ -16,11 +16,15 @@ class HomeViewController: UIViewController {
 
 extension HomeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "HomeCell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = "Issue"
+        content.image = UIImage(systemName: "info.circle")
+        cell.contentConfiguration = content
         return cell
     }
 }
