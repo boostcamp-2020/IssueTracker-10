1b2600af48761f0012cd791a09e680d808927828
hoonv <chaeefe@gmail.com>
2020-11-05 11:06:13 +0900

    chore: add guard statement in estimate cell height
    - 라벨 길이를 측정하는 부분을 guard문으로 변경

--- iOS/IssueTracker/IssueTracker/Issue/IssueList/Controller/IssueViewController+FlowLayout.swift
+++ iOS/IssueTracker/IssueTracker/Issue/IssueList/Controller/IssueViewController+FlowLayout.swift
@@ -11,14 +11,14 @@ extension IssueViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let maxLine: CGFloat = 2
-        let totalWidth = viewModel.issues[indexPath.row].labels.map {
-            $0.title.widthOfString(usingFont: UIFont.systemFont(ofSize: 13, weight: .semibold))
+        guard let item = dataSource.itemIdentifier(for: indexPath) else { return CGSize.zero }
+        let totalWidth = item.labels
+            .map {
+                $0.title.widthOfString(usingFont: UIFont.systemFont(ofSize: 13, weight: .semibold))
             }
-            .map { $0 + CGFloat(40) }
+            .map { $0 + CGFloat(50) }
             .reduce(0) { $0 + $1 }
-        
         let lines = min(maxLine, ceil(totalWidth / view.frame.width))
-        
         return CGSize(width: view.frame.width * 0.9, height: 100 + 36 * lines)
     }
 }
