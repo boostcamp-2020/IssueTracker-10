79388f4caef00f2e9705659fd6ee487ad2cf1469
hoonv <chaeefe@gmail.com>
2020-11-05 11:26:49 +0900

    feat: add temp labels
    - 레이블들이 없어서 임시 레이블들 생성

--- iOS/IssueTracker/IssueTracker/Issue/IssueList/ViewModel/IssueViewModel.swift
+++ iOS/IssueTracker/IssueTracker/Issue/IssueList/ViewModel/IssueViewModel.swift
@@ -8,15 +8,39 @@
 import Foundation
 import hvNetwork
 
+let tempLabels = [Label(id: 0, title: "backend", color: "#111111"),
+                  Label(id: 0, title: "document", color: "#fd3887"),
+                  Label(id: 0, title: "review request", color: "#bd3d21"),
+                  Label(id: 0, title: "frontend", color: "#ce12aa"),
+                  Label(id: 0, title: "refactoring", color: "#bd3d21"),
+                  ]
+
 class IssueViewModel {
     
     let issueManager = IssueManager()
-    var issueDataSource: (([Issue], IssueCriteria? ) -> Void)?
+    var issueApplyToDatasource: (([Issue], IssueCriteria? ) -> Void)?
     var issues: [Issue] = [Issue.empty]
+    var selectedLabels = SelectedLabels.shared
     
+    var inputText = "" {
+        didSet {
+            issueApplyToDatasource?(issues, TitleCriteria(input: inputText))
+        }
+    }
     init() {
         NotificationCenter.default.addObserver(self, selector: #selector(issueDidChanged), name: .issueDidChanged, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(selectedLabelChanged), name: .selectedLabelChanged, object: nil)
         updateIssues()
+    }
+    
+    @objc func selectedLabelChanged() {
+        let filter = selectedLabels.labelsTitle
+            .map { LabelNameCriteria(name: $0) }
+            .reduce( MultiCriteria() , { result, new in
+                result.append(new)
+                return result
+            })
+        issueApplyToDatasource?(issues, filter)
     }
     
     @objc func issueDidChanged() {
@@ -25,9 +49,20 @@ class IssueViewModel {
     
     func updateIssues() {
         issueManager.get { [weak self] issues in
-            self?.issues = issues
+            var temp = issues
+     
+            for i in 0..<temp.count {
+                var left = Int.random(in: 0..<tempLabels.count)
+                var right = Int.random(in: 0..<tempLabels.count)
+                if left > right {
+                    swap(&left, &right)
+                }
+                temp[i].labels += Array(tempLabels[left..<right])
+            }
+            
+            self?.issues = temp
             DispatchQueue.main.async {
-                self?.issueDataSource?(issues, nil)
+                self?.issueApplyToDatasource?(temp, nil)
             }
         }
     }
