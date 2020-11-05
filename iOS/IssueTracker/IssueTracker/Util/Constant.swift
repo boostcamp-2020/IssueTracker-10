74d5a53c3f8c69fe9cdec2013312826151e8e557
hoonv <chaeefe@gmail.com>
2020-11-05 17:35:40 +0900

    feat: create Constant
    - 상수 클래스 생성

---
+++ iOS/IssueTracker/IssueTracker/Util/Constant.swift
@@ -0,0 +1,15 @@
+//
+//  Constant.swift
+//  IssueTracker
+//
+//  Created by 채훈기 on 2020/11/05.
+//
+
+import Foundation
+
+struct Constant {
+    struct Github {
+        static let accessTokenURL = "https://github.com/login/oauth/access_token"
+        static let userAPIURL = "https://api.github.com/user"
+    }
+}
