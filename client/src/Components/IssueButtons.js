34484e29d9648a1979f6af3300fb62e86e8e39d7
수연 <cjg03194@gmail.com>
2020-11-05 16:04:08 +0900

    Refactor: Separate button component
    - 컴포넌트 재사용성을 생각하여 라벨, 마일스톤 버튼과 연두색 버튼 컴포넌트로 분할
    - 버튼에서 공통으로 적용되는 style을 theme으로 설정

--- client/src/Components/IssueButtons.js
+++ client/src/Components/IssueButtons.js
@@ -1,77 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import { LabelIcon, MilestoneIcon } from './icons';
-
-const commonButton = styled.button`
-  outline: none;
-  border-radius: ${(props) => props.theme.radius};
-  color: ${(props) => props.theme.whiteColor};
-  font-size: 18px;
-  font-weight: 700;
-  cursor: pointer;
-`;
+import GreenButton from './GreenButton';
+import LabelMilestoneButton from './LabelMilestoneButton';
 
 const ButtonWrapper = styled.div`
   display: flex;
   justify-content: space-between;
 `;
 
-const Wrapper = styled.div`
-  display: flex;
-`;
-
-const LabelButton = styled(commonButton)`
-  display: flex;
-  align-items: center;
-  height: 60px;
-  padding: 10px 20px;
-  border: 1px solid ${(props) => props.theme.grayColor};
-  border-radius: ${(props) => props.theme.radius} 0 0 ${(props) => props.theme.radius};
-  background-color: ${(props) => props.theme.skyblueColor};
-  svg {
-    fill: ${(props) => props.theme.whiteColor};
-  }
-`;
-
-const MilestoneButton = styled(LabelButton)`
-  border-radius: 0 ${(props) => props.theme.radius} ${(props) => props.theme.radius} 0;
-`;
-
-const ButtonTitle = styled.span`
-  margin-left: 5px;
-`;
-
-const Count = styled.span`
-  display: inline-block;
-  padding: 2px 7px;
-  margin-left: 10px;
-  border-radius: 15px;
-  background-color: ${(props) => props.theme.blueColor};
-  color: ${(props) => props.theme.whiteColor};
-`;
-
-const IssueButton = styled(commonButton)`
-  height: 60px;
-  padding: 10px 30px;
-  background-color: ${(props) => props.theme.greenColor};
-`;
-
 const IssueButtons = () => {
   return (
     <ButtonWrapper>
-      <Wrapper>
-        <LabelButton>
-          <LabelIcon />
-          <ButtonTitle>Labels</ButtonTitle>
-          <Count>3</Count>
-        </LabelButton>
-        <MilestoneButton>
-          <MilestoneIcon />
-          <ButtonTitle>Milestones</ButtonTitle>
-          <Count>10</Count>
-        </MilestoneButton>
-      </Wrapper>
-      <IssueButton>New issue</IssueButton>
+      <LabelMilestoneButton isCount />
+      <GreenButton title="New Issue" />
     </ButtonWrapper>
   );
 };
---
+++ client/src/Components/LabelMilestoneButton.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled from 'styled-components';
+import { LabelIcon, MilestoneIcon } from './icons';
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const LabelButton = styled.button`
+  display: flex;
+  align-items: center;
+  height: 60px;
+  padding: 10px 20px;
+  border: 1px solid ${(props) => props.theme.grayColor};
+  border-radius: ${(props) => props.theme.radius} 0 0 ${(props) => props.theme.radius};
+  background-color: ${(props) => props.theme.skyblueColor};
+  svg {
+    fill: ${(props) => props.theme.whiteColor};
+  }
+`;
+
+const MilestoneButton = styled(LabelButton)`
+  border-radius: 0 ${(props) => props.theme.radius} ${(props) => props.theme.radius} 0;
+`;
+
+const ButtonTitle = styled.span`
+  margin-left: 5px;
+`;
+
+const Count = styled.span`
+  display: inline-block;
+  padding: 2px 7px;
+  margin-left: 10px;
+  border-radius: 15px;
+  background-color: ${(props) => props.theme.blueColor};
+  color: ${(props) => props.theme.whiteColor};
+`;
+
+const LabelMilestoneButton = ({ isCount }) => {
+  if (isCount) {
+    return (
+      <Wrapper>
+        <LabelButton>
+          <LabelIcon />
+          <ButtonTitle>Labels</ButtonTitle>
+          <Count>3</Count>
+        </LabelButton>
+        <MilestoneButton>
+          <MilestoneIcon />
+          <ButtonTitle>Milestones</ButtonTitle>
+          <Count>10</Count>
+        </MilestoneButton>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <LabelButton>
+        <LabelIcon />
+        <ButtonTitle>Labels</ButtonTitle>
+      </LabelButton>
+      <MilestoneButton>
+        <MilestoneIcon />
+        <ButtonTitle>Milestones</ButtonTitle>
+      </MilestoneButton>
+    </Wrapper>
+  );
+};
+
+export default LabelMilestoneButton;
