11dda5ff4adda680e7dffae81804e0b432502fc8
sbyeol3 <ahnsbyeol3@gmail.com>
2020-11-05 19:28:40 +0900

    Refactor : update github login (#106)
    - 깃허브 로그인 로직 수정
    - 필요없는 모듈삭제

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,17 +1,9 @@
 const express = require('express');
-const passport = require('passport');
-const { generateToken } = require('../utils/auth');
+const { loginByGitHub, handleGithubCallback } = require('../services/auth');
 
 const router = express.Router();
 
-router.get('/github/login', passport.authenticate('github'));
-
-router.get('/github', passport.authenticate('github', { session: false }), (req, res) => {
-  const {
-    user: { id, username },
-  } = req;
-  const token = generateToken(id, username);
-  res.status(200).json({ message: 'success', token });
-});
+router.get('/github/login', loginByGitHub);
+router.get('/github', handleGithubCallback);
 
 module.exports = router;
