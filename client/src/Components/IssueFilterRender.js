a489a4a953f4d3436d648d3fb99e98780873657f
sbyeol3 <ahnsbyeol3@gmail.com>
2020-11-05 16:52:55 +0900

    Feat : add modal component for filter (#112)
    - 모달 컴포넌트 추가
    - 재사용을 위해 객체 아이디로 구분하여 렌더링을 다르게
    - 렌더링함수는 길어져서 따로 모듈화

---
+++ client/src/Components/IssueFilterRender.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ModalRow = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px 15px;
+  font-size: 13px;
+  color: ${(props) => props.theme.darkgrayColor};
+`;
+
+const UserAvater = styled.img`
+  width: 20px;
+  height: 20px;
+  margin-right: 15px;
+  border-radius: 100%;
+`;
+
+const Label = styled.span`
+  width: 10px;
+  height: 10px;
+  border-radius: 100%;
+  background-color: ${(props) => props.labelColor};
+`;
+
+const LabelTextWrapper = styled.span`
+  display: flex;
+  flex-direction: column;
+  margin-left: 5px;
+`;
+
+export const renderUsers = (users) => {
+  return users.map((user) => {
+    const { id, username, avatar } = user;
+    return (
+      <ModalRow key={id}>
+        <UserAvater src={avatar} alt={`${username} profile`} />
+        {username}
+      </ModalRow>
+    );
+  });
+};
+
+export const renderLabels = (labels) => {
+  return labels.map((label) => (
+    <ModalRow key={label.id}>
+      <Label labelColor={label.color} />
+      <LabelTextWrapper>{label.title}</LabelTextWrapper>
+    </ModalRow>
+  ));
+};
+
+export const renderMilestones = (milestones) => {
+  return milestones.map((milestone) => <ModalRow key={milestone.id}>{milestone.title}</ModalRow>);
+};
