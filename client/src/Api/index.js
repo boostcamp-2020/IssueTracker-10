20e970025939d2ad4fce1082748b8e4f415be55b
sbyeol3 <ahnsbyeol3@gmail.com>
2020-11-06 11:47:23 +0900

    Fix : fix bugs (#135)
    - dotenv 수정
    - github login 토큰 생성 수정

--- client/src/Api/index.js
+++ client/src/Api/index.js
@@ -27,8 +27,7 @@ export const request = async (config) => {
   try {
     const res = await axios({
       headers: {
-        Authorization:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJzYnllb2wzIiwiaWF0IjoxNjA0NTY1ODM5LCJleHAiOjE2MDQ2NTIyMzl9.tjY-D5afSMBoJt7ocbFu18gDog48uiIV1v6yx_kRKk0',
+        Authorization: config.token,
         'Content-Type': 'application/json',
       },
       ...config,
