5263b0878894fbb30769edf4111c664f42bf0700
Taegyung Gong <gtg92t@gmail.com>
2020-11-09 22:46:10 +0900

    Feat: Add Label List & row component - Label list목록 조회 시 필요한 List component와 row component 추가

---
+++ client/src/Components/LabelList.js
@@ -0,0 +1,40 @@
+import React, { useContext } from 'react';
+import styled from 'styled-components';
+import { LabelStateContext } from '../Context/LabelContext';
+import BoldText from './BoldText';
+import LabelListRow from './LabelListRow';
+
+const LabelListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 1100px;
+  margin: 0 auto;
+  border: ${(props) => props.theme.border};
+  border-radius: ${(props) => props.theme.radiusSmall};
+  background-color: ${(props) => props.theme.whiteColor};
+`;
+
+const LabelHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  background-color: ${(props) => props.theme.skyblueColor};
+  border-top-left-radius: ${(props) => props.theme.radiusSmall};
+  border-top-right-radius: ${(props) => props.theme.radiusSmall};
+`;
+
+const IssueList = ({ labels }) => {
+  const labelState = useContext(LabelStateContext);
+  const headerText = `${labelState.labels.length} labels`;
+  return (
+    <LabelListWrapper>
+      <LabelHeader>
+        <BoldText text={headerText} />
+      </LabelHeader>
+      {labels && labels.map((label) => <LabelListRow key={label.id} label={label} />)}
+    </LabelListWrapper>
+  );
+};
+
+export default IssueList;
