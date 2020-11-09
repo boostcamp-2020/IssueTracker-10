8e7e5cd2953978af2785112adfbee4a7422d2d85
Taegyung Gong <gtg92t@gmail.com>
2020-11-09 22:36:57 +0900

    Feat: Add label list route - label을 조회하고 생성하는 페이지 추가

--- client/src/Routes/index.js
+++ client/src/Routes/index.js
@@ -3,11 +3,13 @@ import { Route } from 'react-router-dom';
 import Main from '../Components/Main';
 import Login from '../Components/Login';
 import Issue from '../Components/Issue';
+import Label from '../Components/Label';
 
 export default () => (
   <>
     <Route exact path="/" component={Main} />
     <Route exact path="/login" component={Login} />
     <Route exact path="/issues" component={Issue} />
+    <Route exact path="/labels" component={Label} />
   </>
 );
