b33a7664966fa84f166d73c4dee764ee9867c5c8
수연 <cjg03194@gmail.com>
2020-11-10 22:53:06 +0900

    Feat: Get open milestones data
    - open된 milestone 데이터를 가져와 milestone context에 저장하도록 기능 추가

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,19 @@ import Router from './Routes';
 import AuthProvider from './Components/Provider/Auth';
 import LabelProvider from './Components/Provider/Label';
 import IssueProvider from './Components/Provider/Issue';
+import MilestoneProvider from './Components/Provider/Milestone';
 
 const App = () => {
   return (
     <AuthProvider>
       <LabelProvider>
-        <IssueProvider>
-          <BrowserRouter>
-            <Router />
-          </BrowserRouter>
-        </IssueProvider>
+        <MilestoneProvider>
+          <IssueProvider>
+            <BrowserRouter>
+              <Router />
+            </BrowserRouter>
+          </IssueProvider>
+        </MilestoneProvider>
       </LabelProvider>
     </AuthProvider>
   );
