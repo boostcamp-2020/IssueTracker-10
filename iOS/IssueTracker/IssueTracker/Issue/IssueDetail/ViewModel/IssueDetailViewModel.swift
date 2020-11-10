08d5c9b51fd771dd710921088cf5c240c09b116a
hoonv <chaeefe@gmail.com>
2020-11-10 13:53:49 +0900

    feat: create viewModel state reactor
    - 뷰모델 생성

---
+++ iOS/IssueTracker/IssueTracker/Issue/IssueDetail/ViewModel/IssueDetailViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  IssueDetailViewModel.swift
+//  IssueTracker
+//
+//  Created by 채훈기 on 2020/11/09.
+//
+
+import Foundation
+
+class IssueDetailViewModel {
+    
+    let reactor: IssueDetailReactor
+    var state: IssueDetailState
+    var updateClosure: ((IssueDetailState) -> Void)?
+        
+    init(reactor: IssueDetailReactor, state: IssueDetailState) {
+        self.reactor = reactor
+        self.state = state
+        reactor.sideEffect = { state in
+            self.state = state
+            self.updateClosure?(state)
+        }
+    }
+    
+    func requestIssueDetail(id: Int) {
+        state = reactor.execute(action: .requestIssueDetail(id), currentState: state)
+        updateClosure?(state)
+    }
+    
+    func requestIssueComment(id: Int) {
+        state = reactor.execute(action: .requestIssueComment(id), currentState: state)
+        updateClosure?(state)
+    }
+}
