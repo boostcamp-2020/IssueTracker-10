111d034265beada8958d66a77634be63b77f843d
kati-kms <qkrguswndk@naver.com>
2020-11-11 15:50:59 +0900

    feat: make MileStone Cell

--- iOS/IssueTracker/IssueTracker/MileStone/MileStoneCollectionViewCell.swift
+++ iOS/IssueTracker/IssueTracker/MileStone/MileStoneCollectionViewCell.swift
@@ -9,7 +9,11 @@ import UIKit
 
 class MileStoneCollectionViewCell: UICollectionViewCell {
 	
+	@IBOutlet weak var titleLabel: UILabel!
+	@IBOutlet weak var descriptionLabel: UILabel!
+	
 	func configure(mileStone: Milestone) {
-		
+		titleLabel.text = mileStone.title
+		descriptionLabel.text = mileStone.description
 	}
 }
