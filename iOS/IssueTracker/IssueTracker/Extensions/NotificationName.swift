9bb176d2d355c01790f11bb01fe0daa2c42c6462
hoonv <chaeefe@gmail.com>
2020-11-12 03:08:44 +0900

    feat: create notification Name Label create remove Edit
    - 레이블의 수정삭제생성에 대해 노티등록

--- iOS/IssueTracker/IssueTracker/Extensions/NotificationName.swift
+++ iOS/IssueTracker/IssueTracker/Extensions/NotificationName.swift
@@ -15,6 +15,10 @@ extension Notification.Name {
 	static let labelDidChanged = Notification.Name.init("labelDidChanged")
     static let commentDidChanged = Notification.Name.init("commentDidChanged")
     static let refreshIssue = Notification.Name.init("refreshIssue")
+    static let EditLabelBegin = Notification.Name.init("EditLabelBegin")
+    static let EditLabelEnd = Notification.Name.init("EditLabelEnd")
+    static let removeLabelOfIssue = Notification.Name.init("removeLabelOfIssue")
+    static let addLabelOfIssue = Notification.Name.init("addLabelOfIssue")
 
 	static let signInDidFinished = Notification.Name.init("signInDidFinished")
 }
