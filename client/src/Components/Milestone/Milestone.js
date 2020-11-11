38e35b92a5cbf2d81139c10db6eb86d500bd2858
Taegyung Gong <gtg92t@gmail.com>
2020-11-11 20:35:33 +0900

    Feat: Add milestone base component - 렌더링 시 open milestone과 closed milestone을 조회하는 base component   추가 - milestone list를 포함하는 component

---
+++ client/src/Components/Milestone/Milestone.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect, useContext } from 'react';
+import styled from 'styled-components';
+import { request } from '../../Api';
+import { AuthStateContext, AuthDispatchContext } from '../../Context/AuthContext';
+import { MilestoneDispatchContext, MilestoneStateContext } from '../../Context/MilestoneContext';
+import { theme } from '../../theme';
+import Button from '../Common/Button';
+import LabelMilestoneButton from '../Common/LabelMilestoneButton';
+import MilestoneList from './MilestoneList';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const RowContainer = styled.div`
+  margin-top: 20px;
+  width: 1100px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const ColumnContainer = styled.div`
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 60%;
+  &:last-child {
+    width: 10%;
+  }
+`;
+
+const Label = ({ token }) => {
+  const authState = useContext(AuthStateContext);
+  const authDispatch = useContext(AuthDispatchContext);
+  const milestoneState = useContext(MilestoneStateContext);
+  const milestoneDispatch = useContext(MilestoneDispatchContext);
+  const [display, setDisplay] = useState(false);
+  const { greenColor } = theme;
+
+  const toggleDisplay = () => {
+    const displayValue = !display;
+    setDisplay(displayValue);
+  };
+
+  useEffect(() => {
+    if (!authState.token) authDispatch({ type: 'LOGIN', token });
+  }, []);
+
+  useEffect(() => {
+    if (authState.token) {
+      const fetchOpenMilestone = async () => {
+        const params = { state: 1 };
+        const config = { url: '/api/milestone', method: 'GET', token: authState.token, params };
+        const { data } = await request(config);
+        if (data) milestoneDispatch({ type: 'GET_OPEN_MILESTONE', data });
+      };
+      const fetchClosedMilestone = async () => {
+        const params = { state: 0 };
+        const config = { url: '/api/milestone', method: 'GET', token: authState.token, params };
+        const { data } = await request(config);
+        if (data) milestoneDispatch({ type: 'GET_CLOSE_MILESTONE', data });
+      };
+      fetchOpenMilestone();
+      fetchClosedMilestone();
+    }
+  }, [authState.token]);
+
+  return (
+    <Wrapper>
+      <RowContainer>
+        <ColumnContainer>
+          <LabelMilestoneButton issueHeader="" />
+        </ColumnContainer>
+        <ColumnContainer>
+          <Button onClick={toggleDisplay} text="New Milestone" color={greenColor} />
+        </ColumnContainer>
+      </RowContainer>
+      <RowContainer>
+        <MilestoneList
+          openMilestone={milestoneState.openMilestone}
+          closedMilestone={milestoneState.closeMilestone}
+        />
+      </RowContainer>
+    </Wrapper>
+  );
+};
+
+export default Label;
