d613a80206408889d6b710ef6515e57e64234626
hoonv <chaeefe@gmail.com>
2020-11-11 14:09:58 +0900

    feat: create collectionview footer view
    - 뷰때매 가려서 footer추가

--- iOS/IssueTracker/IssueTracker/Issue/IssueDetail/Contoroller/IssueDetailViewController+Collection.swift
+++ iOS/IssueTracker/IssueTracker/Issue/IssueDetail/Contoroller/IssueDetailViewController+Collection.swift
@@ -11,8 +11,9 @@ extension IssueDetailViewController {
     func configureCollectionView() {
         collectionView.register(UINib(nibName: "IssueDetailCell", bundle: nil), forCellWithReuseIdentifier: "DetailCell")
         collectionView.register(UINib(nibName: "CollectionHeaderView", bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "DetailHeader")
+        collectionView.register(UINib(nibName: "CollectionFooterView", bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: "DetailFooter")
         if let flowLayout = collectionView?.collectionViewLayout as? UICollectionViewFlowLayout {
-           flowLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+            flowLayout.estimatedItemSize = CGSize(width: self.view.frame.width, height: 120)
          }
     }
 }
@@ -24,12 +25,18 @@ extension IssueDetailViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-    
+        
         if kind == UICollectionView.elementKindSectionHeader {
             guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "DetailHeader", for: indexPath) as? CollectionHeaderView else { return UICollectionReusableView()}
             header.configure(issue: viewModel.state.issue)
             return header
         }
+        
+        if kind == UICollectionView.elementKindSectionFooter {
+            guard let footer = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "DetailFooter", for: indexPath) as? CollectionFooterView else { return UICollectionReusableView()}
+            return footer
+        }
+    
         return UICollectionReusableView()
     }
     
@@ -48,5 +55,9 @@ extension IssueDetailViewController: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: view.frame.width, height: 100)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
+
     }
 }
