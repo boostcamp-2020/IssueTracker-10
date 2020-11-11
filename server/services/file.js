839f328b4f3ca36d1d90d9b64e90820480e7403d
수연 <cjg03194@gmail.com>
2020-11-11 16:38:47 +0900

    Fix: Fix code style
    - 리뷰에 따른 코드 스타일 변경

--- server/services/file.js
+++ server/services/file.js
@@ -1,22 +1,14 @@
 const SUCCESS_MSG = require('./successMessages');
 const ERROR_MSG = require('./errorMessages');
 
-const checkValidation = {
-  files: (fileData) => {
-    const file = fileData;
-    if (!file) return false;
-    return true;
-  },
-};
-
 const uploadFile = async (req, res) => {
   try {
     const { file } = req;
-    if (!checkValidation.files(file)) res.status(400).json({ message: ERROR_MSG.invalid });
+    if (!file) return res.status(400).json({ message: ERROR_MSG.invalid });
     const filePath = process.env.BASE_URL + file.filename;
-    res.status(200).json({ message: SUCCESS_MSG.create, data: filePath });
+    return res.status(200).json({ message: SUCCESS_MSG.create, data: filePath });
   } catch (err) {
-    res.status(500).json({ message: ERROR_MSG.server });
+    return res.status(500).json({ message: ERROR_MSG.server });
   }
 };
 
