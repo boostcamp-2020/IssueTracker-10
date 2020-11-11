22fa2e8818befa3d82df34e1a7ae81cea00b34da
hoonv <chaeefe@gmail.com>
2020-11-11 15:15:26 +0900

    feat: create commentCreateVC
    - 생성 뷰컨 추가

--- iOS/IssueTracker/IssueTracker/Comment/Controller/CommentCreateViewController.swift
+++ iOS/IssueTracker/IssueTracker/Comment/Controller/CommentCreateViewController.swift
@@ -9,7 +9,44 @@ import UIKit
 
 class CommentCreateViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    var issueID: Int!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        placeholderSetting()
+    }
+    
+    func placeholderSetting() {
+        textView.text = "comment"
+        textView.layer.cornerRadius = 10
+        textView.textColor = UIColor.lightGray
+        textView.layer.borderWidth = 2
+        textView.layer.borderColor =
+            UIColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 0.4).cgColor
+    }
+    
+    @IBAction func closeButtonTouched(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func doneButtonTouched(_ sender: Any) {
+//        manager.create(title: title, content: textView.text)
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+extension CommentCreateViewController: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "comment"
+            textView.textColor = UIColor.lightGray
+        }
     }
 }
