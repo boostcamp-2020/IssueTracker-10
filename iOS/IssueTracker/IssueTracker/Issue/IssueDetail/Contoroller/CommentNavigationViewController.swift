ed28e16376c346bef55f68d46044a99119553dbf
hoonv <chaeefe@gmail.com>
2020-11-11 14:09:29 +0900

    feat: crate navigation vc
    - 네비뷰컨 생성

---
+++ iOS/IssueTracker/IssueTracker/Issue/IssueDetail/Contoroller/CommentNavigationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CommentNavigationViewController.swift
+//  IssueTracker
+//
+//  Created by 채훈기 on 2020/11/11.
+//
+
+import UIKit
+
+class CommentNavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
