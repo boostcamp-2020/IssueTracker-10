0363dba417ceff5d6612b88b143280baa22f968d
kati-kms <qkrguswndk@naver.com>
2020-11-11 17:28:24 +0900

    feat: update MilStoneCell
    현재 진행률, open, close 갯수, 날짜 표기

--- iOS/IssueTracker/IssueTracker/MileStone/MileStoneCollectionViewCell.swift
+++ iOS/IssueTracker/IssueTracker/MileStone/MileStoneCollectionViewCell.swift
@@ -11,9 +11,23 @@ class MileStoneCollectionViewCell: UICollectionViewCell {
 	
 	@IBOutlet weak var titleLabel: UILabel!
 	@IBOutlet weak var descriptionLabel: UILabel!
+	@IBOutlet weak var dateLabel: UILabel!
+	@IBOutlet weak var progressLabel: UILabel!
+	@IBOutlet weak var openCountLabel: UILabel!
+	@IBOutlet weak var closeCountLabel: UILabel!
 	
 	func configure(mileStone: Milestone) {
 		titleLabel.text = mileStone.title
 		descriptionLabel.text = mileStone.description
+		dateLabel.text = mileStone.date
+		openCountLabel.text = "\(mileStone.open ?? 0)"
+		closeCountLabel.text = "\(mileStone.closed ?? 0)"
+		progressLabel.text = "\(calculateProgress(open: mileStone.open, close: mileStone.closed))%"
+	}
+	
+	private func calculateProgress(open: Int?, close: Int?) -> Int {
+		guard let open = open, let close = close else { return 0}
+		let percentage: Double = Double(close / (open + close))
+		return Int(ceil(percentage * 100))
 	}
 }
