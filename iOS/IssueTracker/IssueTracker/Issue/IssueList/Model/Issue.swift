835ea284d45d9f2010de641241a041556a017217
kati-kms <qkrguswndk@naver.com>
2020-11-11 15:04:49 +0900

    feat: make MileStone Structure

--- iOS/IssueTracker/IssueTracker/Issue/IssueList/Model/Issue.swift
+++ iOS/IssueTracker/IssueTracker/Issue/IssueList/Model/Issue.swift
@@ -45,11 +45,6 @@ struct Author: Codable, Hashable {
     let avatar: String?
 }
 
-struct Milestone: Codable, Hashable {
-    let id: Int
-    let title: String
-}
-
 struct Assignee: Codable, Hashable {
     let id: Int
     let avatar: String?
