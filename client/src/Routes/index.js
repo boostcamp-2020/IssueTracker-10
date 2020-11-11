748b70dc5d21b45d4268effda3c8ac887a91e00b
Taegyung Gong <gtg92t@gmail.com>
2020-11-11 20:34:04 +0900

    Feat: Add milestone page route & create milestone page route - milestone을 조회하는 페이지 라우트 추가 - milestone을 생성하는 페이지 라우트 추가

--- client/src/Routes/index.js
+++ client/src/Routes/index.js
@@ -7,6 +7,8 @@ import Label from '../Components/Label/Label';
 import NewIssue from '../Components/Issue/NewIssue';
 import Header from '../Components/Common/Header';
 import Search from '../Components/Label/Search';
+import Milestone from '../Components/Milestone/Milestone';
+import MilestoneCreate from '../Components/Milestone/MilestoneCreate';
 
 export default () => (
   <>
@@ -19,5 +21,7 @@ export default () => (
     </Switch>
     <Route exact path="/labels" component={Label} />
     <Route exact path="/search" component={Search} />
+    <Route exact path="/milestones" component={Milestone} />
+    <Route exact path="/milestones/new" component={MilestoneCreate} />
   </>
 );
