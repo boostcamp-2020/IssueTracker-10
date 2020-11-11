9962ae837fdecfd9a5a5e3954554bbf1323d9a0c
kati-kms <qkrguswndk@naver.com>
2020-11-11 18:16:39 +0900

    feat: Configure CollectionView Layout

--- iOS/IssueTracker/IssueTracker/MileStone/MileStoneViewController.swift
+++ iOS/IssueTracker/IssueTracker/MileStone/MileStoneViewController.swift
@@ -18,9 +18,29 @@ class MileStoneViewController: UIViewController {
 		navigationController?.navigationBar.prefersLargeTitles = true
 		navigationController?.setToolbarHidden(false, animated: false)
 		dataSource = MileStoneDiffableDataSource(with: mileStoneCollectionView)
+		mileStoneCollectionView.collectionViewLayout = createLayout()
 	}
 	
 	override func viewWillDisappear(_ animated: Bool) {
 		navigationController?.setToolbarHidden(true, animated: false)
+	}
+	
+	func createLayout() -> UICollectionViewLayout {
+		let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+											 heightDimension: .fractionalHeight(1.0))
+		let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+		let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+											  heightDimension: .absolute(75))
+		let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
+		let spacing = CGFloat(10)
+		group.interItemSpacing = .fixed(spacing)
+
+		let section = NSCollectionLayoutSection(group: group)
+		section.interGroupSpacing = spacing
+		section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 10, bottom: 0, trailing: 10)
+
+		let layout = UICollectionViewCompositionalLayout(section: section)
+		return layout
 	}
 }
