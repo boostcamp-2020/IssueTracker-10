683708478219162b0f993f35e4100342fbdc573d
hoonv <chaeefe@gmail.com>
2020-11-11 14:09:14 +0900

    feat: create comment Create View controller
    - 커멘트 생성 뷰컨 생성

---
+++ iOS/IssueTracker/IssueTracker/Issue/IssueDetail/Contoroller/CommentCreateViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCreateViewController.swift
+//  IssueTracker
+//
+//  Created by 채훈기 on 2020/11/11.
+//
+
+import UIKit
+
+class CommentCreateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
