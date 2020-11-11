b3e877b49f1cf776763dd261cb9dda5b7043c8dc
Taegyung Gong <gtg92t@gmail.com>
2020-11-11 20:32:27 +0900

    Feat: Add progress bar component - 진행률을 볼 수 있는 bar component 추가

---
+++ client/src/Components/Common/ProgressBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: 15px;
+  border: 0;
+  border-radius: ${(props) => props.theme.badgeRadius};
+  background-color: ${(props) => props.theme.skyblueColor};
+  &:after {
+    position: absolute;
+    width: ${(props) => props.percent};
+    height: 15px;
+    border-top-left-radius: ${(props) => props.theme.badgeRadius};
+    border-bottom-left-radius: ${(props) => props.theme.badgeRadius};
+    background-color: ${(props) => props.theme.blueColor};
+    content: '';
+  }
+`;
+
+const ProgressBar = ({ percent, className }) => (
+  <Container percent={percent} className={className} />
+);
+
+export default ProgressBar;
