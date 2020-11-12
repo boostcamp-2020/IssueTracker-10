9d032cbe5932c22cb5749573c501a6a19bcb0201
hoonv <chaeefe@gmail.com>
2020-11-12 17:03:57 +0900

    feat: create LabelOfIssueCreate VC
    - 라벨 생성하는 뷰컨 생성

---
+++ iOS/IssueTracker/IssueTracker/Issue/LabelOfIssue/Controller/LabelOfIssueCreateViewController.swift
@@ -0,0 +1,46 @@
+//
+//  LabelOfIssueCreateViewController.swift
+//  IssueTracker
+//
+//  Created by 채훈기 on 2020/11/12.
+//
+
+import UIKit
+
+class LabelOfIssueCreateViewController: UIViewController {
+   
+    let containerView = UIView()
+    var issue: Issue!
+    var totalLabel: [Label] = []
+    var filteredLabel: [Label] {
+        get { totalLabel.filter{ !issue.labels.contains($0) } }
+    }
+    var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.gray.withAlphaComponent(0.5)
+        setupUI()
+        LabelManager().get(completion: { labels in
+            self.totalLabel = labels
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        })
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.containerView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        UIView.animate(withDuration: 0.16, animations: {
+            self.containerView.transform = .identity
+        })
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        if touch.view != containerView {
+            self.dismiss(animated: false, completion: nil)
+        }
+    }
+}
