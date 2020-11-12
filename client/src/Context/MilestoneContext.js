25f9aaf5c57e28e2d23c25816ed6ea6d4f74933b
Taegyung Gong <gtg92t@gmail.com>
2020-11-12 17:46:55 +0900

    Feat: Add milestone actions get & delete milestone - 특정 마일스톤을 저장하는 action 추가 - open | closed 상태인 마일스톤을 삭제하는 action 추가

--- client/src/Context/MilestoneContext.js
+++ client/src/Context/MilestoneContext.js
@@ -3,6 +3,7 @@ import { createContext } from 'react';
 export const initialMilestoneState = {
   openMilestone: [],
   closeMilestone: [],
+  milestoneDetail: '',
 };
 
 export const milestoneReducer = (state, action) => {
@@ -25,6 +26,22 @@ export const milestoneReducer = (state, action) => {
       const { milestone } = action;
       return {
         openMilestone: [milestone, ...state.openMilestone],
+      };
+    }
+    case 'GET_DETAIL_MILESTONE': {
+      const { milestone } = action;
+      return {
+        milestoneDetail: milestone,
+      };
+    }
+    case 'DELETE_OPEN_MILESTONE': {
+      return {
+        openMilestone: state.openMilestone.filter((milestone) => milestone.id !== action.id),
+      };
+    }
+    case 'DELETE_CLOSED_MILESTONE': {
+      return {
+        closeMilestone: state.closeMilestone.filter((milestone) => milestone.id !== action.id),
       };
     }
     default:
