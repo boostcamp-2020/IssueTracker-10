e54f5e605562eadef2130cfac7544357d4ccc267
hoonv <chaeefe@gmail.com>
2020-11-12 14:25:23 +0900

    feat: create LabelOfIssueCreateVC
    - 라벨 생성하는 뷰컨 생성

---
+++ iOS/IssueTracker/IssueTracker/Issue/IssueDetail/Contoroller/LabelOfIssueCreateViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LabelOfIssueCreateViewController.swift
+//  IssueTracker
+//
+//  Created by 채훈기 on 2020/11/12.
+//
+
+import UIKit
+
+class LabelOfIssueCreateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+}
