75f9439e9e2ff8f760195dca2faafdc2cd49300a
Taegyung Gong <gtg92t@gmail.com>
2020-11-12 18:40:01 +0900

    Feat: Update get random color function - 기준이 달라 폰트 색을 정하는 기준을 하나로 함

--- client/src/utils/color.js
+++ client/src/utils/color.js
@@ -1,27 +1,18 @@
-export const getRandomColor = () => {
-  const standard = '999999';
-
-  const randomHexCode = Math.floor(Math.random() * 16777215).toString(16);
-  const randomColor = `#${randomHexCode}`;
-
-  if (parseInt(`0x${standard}`, 16) > parseInt(`0x${randomHexCode}`, 16)) {
-    const randomFontColor = 'black';
-    return {
-      randomColor,
-      randomFontColor,
-    };
-  }
-  const randomFontColor = 'white';
-  return {
-    randomColor,
-    randomFontColor,
-  };
-};
-
 export const getFontColor = (color) => {
   const r = parseInt(color.substring(1, 3), 16);
   const g = parseInt(color.substring(3, 5), 16);
   const b = parseInt(color.substring(5), 16);
 
   return (r + g + b) / 3 < 112 ? 'white' : 'black';
+};
+
+export const getRandomColor = () => {
+  const randomHexCode = Math.floor(Math.random() * 16777215).toString(16);
+  const randomColor = `#${randomHexCode}`;
+
+  const randomFontColor = getFontColor(randomColor);
+  return {
+    randomColor,
+    randomFontColor,
+  };
 };
