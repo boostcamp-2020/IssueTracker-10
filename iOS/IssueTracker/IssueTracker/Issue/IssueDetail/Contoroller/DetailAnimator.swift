27e21875e8cfa74c8bbd4c5d1f79591d916b97eb
hoonv <chaeefe@gmail.com>
2020-11-12 17:07:56 +0900

    feat: create DetailAnimator for LabelOfIssue
    -  라벨 생성 뷰를 위한 애니메이터 생성

---
+++ iOS/IssueTracker/IssueTracker/Issue/IssueDetail/Contoroller/DetailAnimator.swift
@@ -0,0 +1,68 @@
+//
+//  DetailAnimator.swift
+//  IssueTracker
+//
+//  Created by 채훈기 on 2020/11/12.
+//
+
+import UIKit
+
+class Animator: NSObject, UIViewControllerAnimatedTransitioning {
+
+    static let duration: TimeInterval = 1.25
+
+    private let type: PresentationType
+    private let firstViewController: IssueDetailViewController
+    private let secondViewController: LabelOfIssueCreateViewController
+    
+    init?(type: PresentationType, first: IssueDetailViewController, second: LabelOfIssueCreateViewController) {
+        self.type = type
+        self.firstViewController = first
+        self.secondViewController = second
+    }
+
+
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return Self.duration
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        
+        let containerView = transitionContext.containerView
+
+        guard let toView = secondViewController.view
+            else {
+                transitionContext.completeTransition(false)
+                return
+        }
+
+        toView.alpha = 0
+        containerView.addSubview(toView)
+
+        let temp = secondViewController.containerView
+        temp.backgroundColor = .white
+        containerView.addSubview(temp)
+        
+        UIView.animate(withDuration: Self.duration, animations: {
+            temp.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+        },
+        
+        completion: { _ in
+            temp.removeFromSuperview()
+            toView.alpha = 1
+            temp.transform = .identity
+            transitionContext.completeTransition(true)
+        })
+    }
+}
+
+enum PresentationType {
+
+    case present
+    case dismiss
+
+    var isPresenting: Bool {
+        return self == .present
+    }
+}
+
